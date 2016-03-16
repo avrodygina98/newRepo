a8bf3fa761d2d277bdcd1c5a2717bf299171143e
avrodygina98 <rav@smile26.ru>
2016-03-16 23:48:54 +0300

    что-нибудь)

---
+++ Program3(SQUARE).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Введите число, квадрат которого хотите получить: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Ответ: " + a*a);
+            Console.ReadKey();
+
+
+        }
+    }
+}
